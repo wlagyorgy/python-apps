fe79eb8069f7ba3ecb2cf90be462c0378198323d
wlagyorgy <wlagyorgy@gmail.com>
2024-06-20 00:37:40 +0200

    some mods on todo app

--- todo-app/day_2/web.py
+++ todo-app/day_2/web.py
@@ -15,7 +15,12 @@ st.subheader("This is my todo app.")
 st.write("This is an app to increase your productivity")
 
 for idx, todo in enumerate(todos):
-    st.checkbox(label=todo)
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(idx)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
 
