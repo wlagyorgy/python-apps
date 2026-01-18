31400e2e15dd324d945632b319c0c445f3d539f7
wlagyorgy <wlagyorgy@gmail.com>
2026-01-18 23:23:34 +0100

    student solution & day 2

--- app4-pdf-template/main.py
+++ app4-pdf-template/main.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
@@ -12,7 +13,27 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100,100,100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align="L", ln=1)
-    pdf.line(10,21,200,21)
+
+    # Add lines
+    for y in range(21,298,10):
+        pdf.line(10, y, 200, y)
+
+    # Set the footer
+    pdf.ln(260)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180 ,180,180)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
+
+    for i in range(row['Pages'] - 1):
+        pdf.add_page()
+        # Add lines
+        for y in range(12, 298, 10):
+            pdf.line(10, y, 200, y)
+        # Set the footer
+        pdf.ln(272)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
 pdf.output("output.pdf")
 
