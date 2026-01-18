e2e852fdc94667c686a628665e1b11fb76d832f4
wlagyorgy <wlagyorgy@gmail.com>
2026-01-18 23:46:09 +0100

    1st day of app5

--- app5-news-api-email/main.py
+++ app5-news-api-email/main.py
@@ -1,14 +1,15 @@
 import requests
 
 api_key = "f13d0d99d61946cea6248448e17fba50"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-03&sortBy=publishedAt\
-        &apiKey=f13d0d99d61946cea6248448e17fba50"
+url = f"https://newsapi.org/v2/everything?q=tesla&from=2025-12-23&sortBy=publishedAt\
+        &apiKey={api_key}"
 
 # Make request
 request = requests.get(url)
 
 # Get a dictionary with data
 content = request.json()
+# print(content)
 for article in content["articles"]:
     print(article["title"])
     print(article["description"])
